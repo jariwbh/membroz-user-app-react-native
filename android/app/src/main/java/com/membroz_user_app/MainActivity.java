29bee3873b3a1b219f9239a7bd0fed69ab12e7ce
HarshadJariwala <harshadjariwala1998@gmail.com>
2022-02-22 11:01:21 +0530

    update

--- android/app/src/main/java/com/membroz_user_app/MainActivity.java
+++ android/app/src/main/java/com/membroz_user_app/MainActivity.java
@@ -1,4 +1,4 @@
-package com.staffmembroz;
+package com.cocoontrainer;
 
 import com.facebook.react.ReactActivity;
 
--- android/app/src/main/java/com/membroz_user_app/MainApplication.java
+++ android/app/src/main/java/com/membroz_user_app/MainApplication.java
@@ -1,4 +1,4 @@
-package com.staffmembroz;
+package com.cocoontrainer;
 
 import android.app.Application;
 import android.content.Context;
