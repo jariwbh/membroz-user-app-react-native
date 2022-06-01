ca6f72d4764df81c67fc6076761e08f040f37743
HarshadJariwala <harshadjariwala1998@gmail.com>
2022-06-01 15:41:45 +0530

    update

--- src/services/FreshLeadService/FreshLeadService.js
+++ src/services/FreshLeadService/FreshLeadService.js
@@ -3,10 +3,42 @@ import Axios from '../../helpers/appConfig';
 export const FreshLeadService = (id) => {
     const body = {
         "search": [
-            { "searchfield": "status", "searchvalue": "active", "criteria": "eq", "datatype": "text" },
-            { "searchfield": "handlerid", "searchvalue": id, "criteria": "eq", "datatype": "ObjectId" },
-            { "searchfield": "transactions", "searchvalue": Number(0), "criteria": "eq", "datatype": "number", "cond": "or" },
-            { "searchfield": "transactions", "searchvalue": false, "criteria": "exists", "datatype": "boolean", "cond": "or" }
+            {
+                "searchfield": "status",
+                "searchvalue": "active",
+                "criteria": "eq",
+                "datatype": "text"
+            },
+            {
+                "searchfield": "handlerid",
+                "searchvalue": id,
+                "criteria": "eq",
+                "datatype": "ObjectId"
+            },
+            {
+                "searchfield": "transactions",
+                "searchvalue": Number(0),
+                "criteria": "eq",
+                "datatype": "number",
+                "cond": "or"
+            },
+            {
+                "searchfield": "transactions",
+                "searchvalue": false,
+                "criteria": "exists",
+                "datatype": "boolean",
+                "cond": "or"
+            }
+        ],
+        "select": [
+            {
+                "fieldname": "property",
+                "value": "1"
+            },
+            {
+                "fieldname": "createdAt",
+                "value": "1"
+            }
         ], "formname": "mypromotion"
     }
     return Axios.post('enquiries/filter', body);
@@ -15,8 +47,28 @@ export const FreshLeadService = (id) => {
 export const MyLeadService = (id) => {
     const body = {
         "search": [
-            { "searchfield": "status", "searchvalue": "active", "criteria": "eq", "datatype": "text" },
-            { "searchfield": "handlerid", "searchvalue": id, "criteria": "eq", "datatype": "ObjectId" },
+            {
+                "searchfield": "status",
+                "searchvalue": "active",
+                "criteria": "eq",
+                "datatype": "text"
+            },
+            {
+                "searchfield": "handlerid",
+                "searchvalue": id,
+                "criteria": "eq",
+                "datatype": "ObjectId"
+            },
+        ],
+        "select": [
+            {
+                "fieldname": "property",
+                "value": "1"
+            },
+            {
+                "fieldname": "createdAt",
+                "value": "1"
+            }
         ], "formname": "mypromotion"
     }
     return Axios.post('enquiries/filter', body);
