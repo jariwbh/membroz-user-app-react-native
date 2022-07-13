28365e145bdc60567af1b2e5620f4c5be3a8a772
HarshadJariwala <harshadjariwala1998@gmail.com>
2022-07-13 17:15:14 +0530

    update

--- src/services/FollowUpService/FollowUpService.js
+++ src/services/FollowUpService/FollowUpService.js
@@ -1,5 +1,6 @@
 import Axios from '../../helpers/appConfig';
 
+
 export const followUpService = (userID, filterValue) => {
     let body
     if (userID != null && userID != undefined && filterValue != null && filterValue != undefined) {
@@ -47,3 +48,52 @@ export const followUpService = (userID, filterValue) => {
     }
     return Axios.post('activities/filter', body)
 }
+
+
+// export const followUpService = (userID, filterValue, pageno, sizeno) => {
+//     let body
+//     if (userID != null && userID != undefined && filterValue != null && filterValue != undefined) {
+//         body =
+//         {
+//             "search": [
+//                 {
+//                     "searchfield": "status",
+//                     "searchvalue": "followup",
+//                     "criteria": "eq",
+//                     "datatype": "text"
+//                 },
+//                 {
+//                     "searchfield": "assingeeuser",
+//                     "searchvalue": userID,
+//                     "criteria": "eq",
+//                     "datatype": "ObjectId"
+//                 },
+//                 {
+//                     "searchfield": "property.interest",
+//                     "searchvalue": filterValue,
+//                     "criteria": "eq",
+//                     "datatype": "text"
+//                 }
+//             ], "sort": { "duedate": -1 }, "pageNo": pageno.toString(), "size": sizeno.toString()
+//         }
+//     } else {
+//         body =
+//         {
+//             "search": [
+//                 {
+//                     "searchfield": "status",
+//                     "searchvalue": "followup",
+//                     "criteria": "eq",
+//                     "datatype": "text"
+//                 },
+//                 {
+//                     "searchfield": "assingeeuser",
+//                     "searchvalue": userID,
+//                     "criteria": "eq",
+//                     "datatype": "ObjectId"
+//                 }
+//             ], "sort": { "duedate": -1 }, "pageNo": pageno.toString(), "size": sizeno.toString()
+//         }
+//     }
+//     return Axios.post('activities/filter', body)
+// }
